021bb2516bdf816a89dae20b334a68a933f82ef0
sharanr91 <sharanr91@Dear.General.github.com>
2020-03-06 09:03:13 +0800

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'angular-reddit';
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    //title and link are both objects of type HTMLInputElement and not values directly
+    console.log(
+      `Adding article title : ${title.value} and link: ${link.value}`
+    );
+    return false;
+  }
 }
